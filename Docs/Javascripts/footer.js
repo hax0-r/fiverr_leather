8d00530b38fe397782697ad6b86a1adcb4d65a9b
Muhammad Talha <talha18513@gmail.com>
2025-03-29 18:32:12 +0500

    slide bar

---
+++ Docs/Javascripts/footer.js
@@ -0,0 +1,39 @@
+const footer = document.getElementById("footer");
+
+footer.innerHTML = `
+<footer class="w-full mt-16 md:mt-28">
+            <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <!--Grid-->
+                <div
+                    class="grid grid-cols-1 sm:grid-cols-4 lg:grid-cols-5 gap-3 md:gap-8 py-10 max-sm:max-w-sm max-sm:mx-auto gap-y-8">
+                    <div class="mb-10 lg:col-span-2 lg:mb-0">
+                        <a href="./index.html" class="flex justify-center lg:justify-start">
+                            <img src="./Docs/Images/logo.png" class="h-20" alt="logo">
+                        </a>
+                        <p class="py-8 text-sm text-gray-800 lg:max-w-xs text-center lg:text-left"> Excellence in
+                            Leather Tanning Since 1952</p>
+                    </div>
+                    <!--End Col-->
+                    <!--End Col-->
+                    <div class="lg:mx-auto text-left ">
+                        <h4 class="text-lg text-[#000] font-medium mb-5">Links</h4>
+                        <ul class="text-sm  transition-all duration-500">
+                            <li class="mb-4"><a href="./index.html" class="text-gray-600 hover:underline">Home</a></li>
+                            <li class="mb-4"><a href="./about-us.html" class="text-gray-600 hover:underline">About
+                                    Us</a></li>
+                            <li class="mb-4"><a href="./products.html"
+                                    class="text-gray-600 hover:underline">Products</a></li>
+                        </ul>
+                    </div>
+
+                </div>
+                <!--Grid-->
+                <div class="py-7 border-t border-gray-200">
+                    <div class="flex items-center justify-center">
+                        <span class="text-sm text-gray-400 ">©<a href="./index.html">AIT Industries</a> 2025, All rights
+                            reserved.</span>
+                    </div>
+                </div>
+            </div>
+        </footer>
+`
---
+++ Docs/Javascripts/navbar.js
@@ -0,0 +1,58 @@
+const navbar = document.getElementById("navbar");
+
+navbar.innerHTML = `
+         <nav>
+            <div class="max-w-7xl mx-auto flex justify-between items-center w-full p-5">
+                <a href="./index.html" class="flex justify-center lg:justify-start">
+                    <img src="./Docs/Images/logo.png" class="h-14" alt="logo">
+                </a>
+                <ul class="md:flex hidden items-center gap-7">
+                    <li><a href="./index.html" class="hover:text-[#000] transition-all duration-500">Home</a></li>
+                    <li><a href="./about-us.html" class="hover:text-[#000] transition-all duration-500">About</a>
+                    </li>
+                    <li><a href="./products.html" class="hover:text-[#000] transition-all duration-500">Products</a>
+                    </li>
+                </ul>
+                <i id="mobileMenuBtn" class="fa-solid fa-bars md:hidden text-2xl cursor-pointer"></i>
+            </div>
+        </nav>
+
+        <div id="mobileMenuBg"
+            class="fixed cursor-crosshair transition-all duration-500 top-0 right-0 w-0 h-full bg-[#0d0d0c] opacity-50 z-40">
+        </div>
+        <div id="mobileMenu"
+            class="fixed right-0 top-0 bg-black max-w-[30rem] w-0 overflow-hidden h-screen z-50 transition-all duration-500">
+            <i id="mobileMenuCloseBtn"
+                class="fa-solid fa-x z-50 relative flex items-end justify-end p-5 text-2xl cursor-pointer"></i>
+            <ul class="flex relative z-50 flex-col p-10 gap-7">
+                <li><a href="./index.html" class="hover:text-[#000] transition-all font-medium duration-500">Home</a>
+                </li>
+                <li><a href="./about-us.html"
+                        class="hover:text-[#000] transition-all font-medium duration-500">About</a></li>
+                <li><a href="./products.html"
+                        class="hover:text-[#000] transition-all font-medium duration-500">Products</a>
+                </li>
+            </ul>
+        </div>
+`
+
+
+const mobileMenuBtn = document.getElementById("mobileMenuBtn");
+const mobileMenu = document.getElementById("mobileMenu");
+const mobileMenuCloseBtn = document.getElementById("mobileMenuCloseBtn");
+const mobileMenuBg = document.getElementById("mobileMenuBg");
+
+mobileMenuBtn.addEventListener("click", () => {
+    mobileMenu.style.width = "100%";
+    mobileMenuBg.style.width = "100vw";
+});
+
+mobileMenuCloseBtn.addEventListener("click", () => {
+    mobileMenu.style.width = "0";
+    mobileMenuBg.style.width = "0";
+});
+
+mobileMenuBg.addEventListener("click", () => {
+    mobileMenu.style.width = "0";
+    mobileMenuBg.style.width = "0";
+});
